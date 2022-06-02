0700c6fcf5758c1c7f33f1c90427d693bfeb235c
VendoAU <reillyreaby@gmail.com>
2022-06-02 19:41:48 +1000

    Switched gradle to kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    java
+}
+
+group = "com.vendoau"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    compileOnly("com.github.Minestom:Minestom:529_extension_improvement-SNAPSHOT")
+}
