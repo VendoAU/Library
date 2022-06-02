7adc327472371be8703461ecb8054208dd9b9d13
VendoAU <reillyreaby@gmail.com>
2022-06-02 20:34:47 +1000

    jitpack.yml didn't work, hopefully this will

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,3 +15,6 @@ repositories {
 dependencies {
     compileOnly("com.github.Minestom:Minestom:529_extension_improvement-SNAPSHOT")
 }
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
